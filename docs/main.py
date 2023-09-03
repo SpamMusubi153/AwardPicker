b18c1cd5a74fda796ec2b349a13d88674e88d1db
Max <76268730+SpamMusubi153@users.noreply.github.com>
2023-09-03 10:09:44 -0700

    Refactor the code and remove logic errors

--- docs/main.py
+++ docs/main.py
@@ -11,28 +11,16 @@ from browser.local_storage import storage
 # TODO: Later, configure the program to automatically load previously stored data.
 window.localStorage.clear()
 
-# Variables to store results
-# Storage Format = {
-#                   Specialist Name : [List of Records],
-# 
-#                  }
-sorted_by_specialist = {}
-storage["sorted_by_specialist_chart_created"] = "False"
-
-# Storage Format = {
-#                   Class Name : [List of Records],
-#                  }
-sorted_by_class = {}
-storage["sorted_by_class_chart_created"] = "False"
-
+# Store the month and year currently selected for display by the user
 storage["current_app_month"] = str(0)
 storage["current_app_year"] = str(0)
 
-storage["number_of_classes"] = str(0)
+# The first graph created for each uploaded file should have its month and year automatically determined.
+storage["first_graph_from_file"] = str("True")
 
 
 
-def retrieve_spreadsheet(event):
+def retrieve_file(event=None):
 
     def display_file_error(message=None, remedy=None):
         document["file_error_dialog"].style.display = "block"
@@ -48,7 +36,7 @@ def retrieve_spreadsheet(event):
     filesize = document["spreadsheet_upload"].files[0].size
 
     # Validate the file
-    # For performance, if the file is greater than 3MB, display an error and request a smaller file.
+    # To stay within localstorage bounds, display an error if the file size is greater than 2MB.
     if filesize > 2000000:
         display_file_error("Unfortunately, this file is too big!", "Please try making a copy of the file in Excel that only contains data from the most recent month.")
         return
@@ -67,20 +55,19 @@ def retrieve_spreadsheet(event):
     def on_read_load(event):
         spreadsheet = event.target.result
 
+        storage["first_graph_from_file"] = str("True")
         document["data"] = spreadsheet
         
-        process_csv_file(spreadsheet)
+        process_file()
 
     # Read the file
     reader = window.FileReader.new()
     reader.readAsText(document["spreadsheet_upload"].files[0])
     reader.bind("load", on_read_load)
 
+document["spreadsheet_upload"].bind("change", retrieve_file)
 
-
-document["spreadsheet_upload"].bind("change", retrieve_spreadsheet)
-
-
+# Unfortunately, dataclasses are not implemented by Brython. This may change on a future date.
 # @dataclass
 class Record:
     month : int
@@ -94,7 +81,118 @@ class Record:
         self.year = year
         self.day = day
         self.specialist = specialist
-        self.class_name = class_name
+        self.class_name = class_name 
+
+def process_file():
+
+    # Variables to store results
+    # Storage Format = {
+    #                   Specialist Name : [List of Records],
+    # 
+    #                  }
+    sorted_by_specialist = {}
+
+    # Storage Format = {
+    #                   Class Name : [List of Records],
+    #                  }
+    sorted_by_class = {}
+
+    assert "data" in storage.keys(), "A spreadsheet file under the \"data\" key could not be found in localstorage."
+
+    # Retrieve the file from localstorage and decode it into text.
+    spreadsheet = storage["data"]
+    spreadsheet = base64.b64decode(spreadsheet).decode("utf-8")
+
+
+    # A function to sort records into dictionaries organized by specialist and class name.
+    def add_result(record : Record):
+
+        # Add the record to the sorted_by_specialist dictionary.
+        if record.specialist in sorted_by_specialist.keys():
+            sorted_by_specialist[record.specialist].append(record)
+
+        # If the current specialist key does not exist, create it before appending the record.
+        else:
+            sorted_by_specialist[record.specialist] = []
+            sorted_by_specialist[record.specialist].append(record)
+
+
+        # Add the result to the sorted_by_class dictionary.
+        if record.class_name in sorted_by_class.keys():
+            sorted_by_class[record.class_name].append(record)
+
+        # If the current class name key does not exist, create it before appending the record.
+        else:
+            sorted_by_class[record.class_name] = []
+            sorted_by_class[record.class_name].append(record)
+            
+
+    # Split the rows from the text by line.
+    rows = spreadsheet.split("\n")
+
+    # Process every row of the provided CSV File
+    for i, row in enumerate(rows):
+
+        # Remove any quotation characters that separate teacher names
+        row = row.replace('"', "")
+        row = row.replace('\r', "")
+
+
+        # Skip over the header row
+        if i == 0:
+            continue
+
+        # Retrieve the columns by splitting the CSV file by comma (,).
+        columns = row.split(",")
+
+        # If the columns contain a valid amount of data, extract its data.
+        if len(columns) > 2:
+            current_date = None
+            current_specialist = None
+
+            for j, column in enumerate(columns):
+
+                # Extract the date from the first column of each row that applies to all data in the row.
+                if j == 0:                
+                    current_date = datetime.strptime(column, "%m/%d/%Y %H:%M:%S")
+
+                    # If this is the first time creating a graph from the uploaded file, automatically pick the latest month and year.
+                    if storage["first_graph_from_file"] == "True":
+
+                        # Save the most recent year and month to display it in the first generated graphs. 
+                        current_year = current_date.year
+                        current_month = current_date.month
+
+                        # If the current year is the newest year, save the year, and indicate that the month needs to be updated too.
+                        if current_year > int(storage["current_app_year"]):
+                            storage["current_app_year"] = str(current_year)
+                            storage["current_app_month"] = str(0)
+
+                        if current_month > int(storage["current_app_month"]):
+                            storage["current_app_month"] = str(current_month)
+
+                # Extract the specialist name in the second column that applies to all data in the row.
+                elif j == 1:
+                    current_specialist = column
+
+                else:
+
+                    # If the column is empty, pass over it.
+                    if column == "":
+                        continue
+
+                    # Remove any extraneous spaces in the column.
+                    column.replace(" ", "")
+                    
+                    # Create and save a record of the 
+                    current_record = Record(current_date.month, current_date.year, current_date.day, current_specialist, column)
+                    add_result(current_record)
+
+                    # Keep track of the number of classes in the data.
+                    storage["number_of_classes"] = str(int(storage["number_of_classes"]) + 1)
+
+    storage["first_graph_from_file"] = "False"
+    _display_results(sorted_by_specialist, sorted_by_class)
 
 
 def create_bar_chart(canvas_id, bar_labels, bar_data, bar_metric_name):
@@ -124,93 +222,7 @@ def create_bar_chart(canvas_id, bar_labels, bar_data, bar_metric_name):
         }
     })
 
- 
-
-def process_csv_file(spreadsheet=None):
-
-    if "data" in storage.keys():
-        spreadsheet = storage["data"]
-        spreadsheet = base64.b64decode(spreadsheet).decode("utf-8")
-
-    def add_result(record : Record):
-
-        # Add the result to the sorted_by_specialist dictionary.
-        if record.specialist in sorted_by_specialist.keys():
-            sorted_by_specialist[record.specialist].append(record)
-
-        # If the current specialist key does not exist, create it now.
-        else:
-            sorted_by_specialist[record.specialist] = []
-            sorted_by_specialist[record.specialist].append(record)
-
-
-
-        # Then, add the result to the sorted_by_class dictionary.
-        if record.class_name in sorted_by_class.keys():
-            sorted_by_class[record.class_name].append(record)
-
-        # If necessary, create the class name key list.
-        else:
-            sorted_by_class[record.class_name] = []
-            sorted_by_class[record.class_name].append(record)
-            
-
-    rows = spreadsheet.split("\n")
-
-    for i, row in enumerate(rows):
-
-        # Remove any quotation characters that separate teacher names
-        row = row.replace('"', "")
-        row = row.replace('\r', "")
-
-
-        # Skip over the header row
-        if i == 0:
-            continue
-
-        columns = row.split(",")
-        if len(columns) > 1:
-            current_date = None
-            current_specialist = None
-
-            for j, column in enumerate(columns):
-
-                # Extract the date from the first column of each row.
-                if j == 0:                
-                    current_date = datetime.strptime(column, "%m/%d/%Y %H:%M:%S")
-
-                    if storage["sorted_by_class_chart_created"] == "False":
-                        # Save the most recent year and month to display it in the first generated graphs. 
-                        current_year = current_date.year
-                        current_month = current_date.month
-                        # If the current year is the newest year, save the year, and indicate that the month needs to be updated too.
-                        if current_year > int(storage["current_app_year"]):
-                            storage["current_app_year"] = str(current_year)
-                            storage["current_app_month"] = str(0)
-
-                        if current_month > int(storage["current_app_month"]):
-                            storage["current_app_month"] = str(current_month)
-
-                elif j == 1:
-                    current_specialist = column
-
-                else:
-                    if column == "":
-                        continue
-
-                    # Remove any extraneous spaces.
-                    column.replace(" ", "")
-                    
-                    current_record = Record(current_date.month, current_date.year, current_date.day, current_specialist, column)
-                    add_result(current_record)
-
-                    # Keep track of the number of classes in the data.
-                    storage["number_of_classes"] = str(int(storage["number_of_classes"]) + 1)
-
-    display_results()
-
-
-def display_results(event=None):
+def _display_results(sorted_by_specialist, sorted_by_class):
 
 
     # Graph the results by class
@@ -220,39 +232,40 @@ def display_results(event=None):
     current_data = []
     for key in bar_labels:
         current_key_data = sorted_by_class[key]
+        current_class_success_count = 0
 
         for record in current_key_data:
-            if record.month == int(storage["current_app_month"]) and record.year == int(storage["current_app_year"]):
-                continue
+
+            # If the record 
+            if (record.month == int(storage["current_app_month"])) and (record.year == int(storage["current_app_year"])):
+                current_class_success_count += 1
             else:
-                current_key_data.remove(record)
-        current_data.append(current_key_data)
+                continue
 
+        current_data.append(current_class_success_count)
 
-    # Tally the data for the current month
-    bar_data = [len(current_records_list) for current_records_list in current_data]
+    bar_data = current_data
 
+    # The title of the current graph
     bar_metric_name = "Number of 4-Star or 5-Star Days in Specialists"
 
+    # Retrieve the full month name
     month = datetime(int(storage['current_app_year']), int(storage['current_app_month']), 1).strftime("%B")
 
-    document["currentMonth"].textContent = f"{month}, {storage['current_app_year']}"
 
     SORTED_BY_SPECIALIST_CHART_ID = "byClassChart"
-
-    # If the chart was not previously created, create it now
-    if storage["sorted_by_specialist_chart_created"] == "False":
-        create_bar_chart(SORTED_BY_SPECIALIST_CHART_ID, bar_labels, bar_data, bar_metric_name)
-        storage["sorted_by_specialist_chart_created"] = "True"
-    
-    # Otherwise, update the already created chart.
-    else:
+    # If the chart already exists, update its data
+    try:
         chart = window.Chart.getChart(SORTED_BY_SPECIALIST_CHART_ID)
-        chart.destroy()
+        chart.data.datasets[0] = bar_data
+        chart.update()
 
+    # Otherwise, create the chart from scratch
+    except:
         create_bar_chart(SORTED_BY_SPECIALIST_CHART_ID, bar_labels, bar_data, bar_metric_name)
-        # chart.data.datasets[0] = bar_data
-        # chart.update()
+
+    # Update the app to display the current month and year of the chart.
+    document["currentMonth"].textContent = f"{month}, {storage['current_app_year']}"
 
 
 
@@ -264,7 +277,7 @@ def previous_month_handler(event=None):
     else:
         storage["current_app_month"] = str(int(storage["current_app_month"]) - 1)
 
-    process_csv_file()
+    process_file()
 
 document["previousMonthButton"].bind("click", previous_month_handler)
 
@@ -276,6 +289,6 @@ def next_month_handler(event=None):
     else:
         storage["current_app_month"] = str(int(storage["current_app_month"]) + 1)
 
-    process_csv_file()
+    process_file()
 
 document["nextMonthButton"].bind("click", next_month_handler)
