851e644f7fc7f857037d53cacbde84847920b309
Max <76268730+SpamMusubi153@users.noreply.github.com>
2023-09-03 10:46:47 -0700

    Update all data instead of the data for the first bar

--- docs/main.py
+++ docs/main.py
@@ -268,7 +268,7 @@ def _display_results(sorted_by_specialist, sorted_by_class):
     # If the chart already exists, update its data
     try:
         chart = window.Chart.getChart(SORTED_BY_SPECIALIST_CHART_ID)
-        chart.data.datasets[0].data[0] = bar_data
+        chart.data.datasets[0].data = bar_data
         chart.update()
 
     # Otherwise, create the chart from scratch
